59ac5394cb3f160f51304f0df6c7cc812dfa751f
Whizark <devaloka@whizark.com>
2015-12-10 08:50:54 +0900

    fix: PluginInterface::boot() isn't invoked

--- src/Provider/QueryExporterProvider.php
+++ src/Provider/QueryExporterProvider.php
@@ -109,5 +109,7 @@ class QueryExporterProvider implements
         if ($plugin instanceof ActivatablePluginInterface) {
             $plugin->register();
         }
+
+        $plugin->boot();
     }
 }
